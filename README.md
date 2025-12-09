a921380a1af1f21d327f10108872d5c1f52f883d
muyangyan <muyangyan1@gmail.com>
2025-12-09 17:47:15 -0500

    readme

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 
 prism bomb-in-toilet.prism -pctl $'Pmax=? [F bomb_defused & !toilet_clogged]'
 
+
+
+
+if policy does not propose an applicable action, we are just stuck. no contingencies.
--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fire import Fire
 
-from pddl_parser import pddl_to_mdp
+
+def pddl_to_mdp(domain_file: str, problem_file: str) -> str:
+    return ""
+
+
+def mdp_to_dtmc(mdp_file: str, policy: str) -> str:
+    return ""
+
+def verify_property(dtmc_file: str, property: str) -> str:
+    """
+    dtmc_file: Path to the DTMC model file in PRISM format.
+    property: path to file containing property in 
+    """
+    # call prism on dtmc file, with property
+
+    return ""
 
 
 
-
-
-
-def main(
+def run_single(
     domain_file: str,
     problem_file: str,
     policy: str, 
@@ -15,5 +27,28 @@ def main(
 ):
 
 
+    # parse pddl domain, problem -> mdp file
+    mdp_text = pddl_to_mdp(domain_file, problem_file)
+    # write mdp file to disk
+    with open("tmp/mdp.prism", "w") as f:
+        f.write(mdp_text)
+
+
+    # apply policy to get actions -> dtmc file
+    dtmc_text = mdp_to_dtmc(mdp_text, policy)
+    # write dtmc file to disk
+    with open("tmp/dtmc.prism", "w") as f:
+        f.write(dtmc_text)
+
+    # call prism on dtmc file, with property
+    result = verify_property("tmp/dtmc.prism", property)
+
+    # print result
+    print(result)
+
+def main():
+    pass
+
+
 if __name__ == "__main__":
-    Fire(main)
+    Fire(run_single)
