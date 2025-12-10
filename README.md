6f82e468058eec3c90573a7c6e342ba394f636e9
Mikk Sanborn <sanbornmikk@gmail.com>
2025-12-09 22:31:50 -0500

    First implementation of verify_property; update proof_of_concepts dtmc

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Verifying Properties of Probabilistic Planning Policies
 
+Running MDP version:
+prism bomb-in-toilet-mdp.prism -pctl $'Pmax=? [F bomb_defused & !toilet_clogged]'
+or for the encoded "goal",
+prism bomb-in-toilet-mdp.prism -pctl $'Pmax=? [F bomb_defused & !toilet_clogged]'
 
-prism bomb-in-toilet.prism -pctl $'Pmax=? [F bomb_defused & !toilet_clogged]'
-
+Running DTMC version (using the "goal" as the property):
+prism bomb-in-toilet.dtmc -pctl $'P=? [F "goal"]'
 
 
 
 if policy does not propose an applicable action, we are just stuck. no contingencies.
+
+Note:
+Current implementation of `verify_property` uses a call to `subprocess.run()`, which is inherently unsafe. Do not run this code on inputs you do not trust!
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fire import Fire
 import json
 import plado
+import subprocess
 
 def pddl_to_mdp(domain_file: str, problem_file: str) -> str:
     domain, problem = plado.parse(domain_file, problem_file)
@@ -19,9 +20,36 @@ def verify_property(dtmc_file: str, property_file: str) -> str:
     dtmc_file: Path to the DTMC model file in PRISM format.
     property_file: path to file containing property in 
     """
-    # call prism on dtmc file, with property
+    # call prism on dtmc file, with property speicified in the file
+    with open(property_file, "r") as property_infile:
+        property = property_infile.read().strip()
 
-    return ""
+    # TODO -- change tmp/results.txt to wherever it should be instead
+    results_file = "tmp/results.txt"
+
+    # Prepare command for subprocess
+    command = ["prism", dtmc_file, "-pctl", f"$'Pmax=? [{property}]'", f"-exportresults {results_file}"]
+    output_data = subprocess.run(command, capture_output=True, text=True)
+
+    # Check return code
+    if output_data.returncode != 0:
+        print(f"PRISM returned {output_data.returncode} on inputs `{command}` with error:\n{output_data.stderr}\nAborting verify_property.")
+        return output_data.stderr
+    
+    # TODO perform post-processing:
+    # - Perform error checking (e.g. the model may be incomplete, or there may be warnings about its structure, or certain states are unreachable or unactionable)
+    # - Return results if there are no errors
+
+    # Get command output (if needed)
+    # output_str = output_data.stdout
+    
+    # Get results from results file
+    # Note: results files are output as "Result\nn.nn\n", hence we only need the second line
+    with open(results_file, "r") as results_infile:
+        results_raw = results_infile.read().strip()
+        output_amount = results_raw.split()[1].strip()
+
+    return output_amount
 
 
 
