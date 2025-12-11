9b7bbf3abc4568424ab8d177f72ee990114b599d
Sean Zhang <zhan4184@purdue.edu>
2025-12-11 15:29:59 -0500

    debug prints in main

--- main.py
+++ main.py
@@ -10,10 +10,17 @@ def main(
     policy_files = [f for f in os.listdir(domain_dir) if f.endswith(".json")]
     property_files = [f for f in os.listdir(domain_dir) if f.endswith(".pctl")]
 
+    print('DOMAIN DIR:', domain_dir)
+    print(problem_files)
+    print(policy_files)
+    print(property_files)
+
     for problem_file in problem_files:
         for policy_file in policy_files:
             for property_file in property_files:
+                print('RUNNING FOR:', problem_file, policy_file, property_file)
                 result = run_single(domain_dir, problem_file, policy_file, property_file)
     
 if __name__ == "__main__":
     Fire(main)
+
