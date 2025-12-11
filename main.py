3d53f224c861b17b9fe93ee6d6c0015624624378
muyangyan <muyangyan1@gmail.com>
2025-12-11 14:40:30 -0500

    refactor and change args

--- main.py
+++ main.py
@@ -1,77 +1,20 @@
-from fire import Fire
-import json
-import plado
-import subprocess
-from src.pddl_parser import pddl_to_mdp, PPDDLToPRISM
 import os
+from fire import Fire
+from run import run_single
 
-def verify_property(dtmc_file: str, property_file: str) -> str:
-    """
-    dtmc_file: Path to the DTMC model file in PRISM format.
-    property_file: path to file containing property in 
-    """
-    # call prism on dtmc file, with property speicified in the file
-    with open(property_file, "r") as property_infile:
-        property = property_infile.read().strip()
-
-    results_file = "tmp/results.txt"
-    if not os.path.exists(results_file):
-        with open(results_file, "w") as f:
-            f.write("")
-
-    # Prepare command for subprocess
-    command = ["prism", dtmc_file, "-pctl", f"P=? [{property}]", f"-exportresults", f"{results_file}"]
-    command_text = " ".join(command)
-    print('COMMAND TEXT:', command_text)
-    output_data = subprocess.run(command, capture_output=True, text=True)
-
-    # Check return code
-    if output_data.returncode != 0:
-        print(f"PRISM returned {output_data.returncode} on inputs `{command}` with error:\n{output_data.stderr}\nAborting verify_property.")
-        print('ERROR:', output_data.stderr)
-        print('OUTPUT:', output_data.stdout)
-        return None
-    
-    # TODO perform post-processing:
-    # - Perform error checking (e.g. the model may be incomplete, or there may be warnings about its structure, or certain states are unreachable or unactionable)
-    # - Return results if there are no errors
-
-    with open(results_file, "r") as results_infile:
-        results_raw = results_infile.read().strip()
-        output_amount = results_raw.split()[1].strip()
-
-    return output_amount
-
-
-def run_single(
-    domain_file: str = "data/deterministic/blocksworld/domain.pddl",
-    problem_file: str = "data/deterministic/blocksworld/1.pddl",
-    policy_file: str = "data/deterministic/blocksworld/all_on_table.json", 
-    property_file: str = "data/deterministic/blocksworld/property.pctl",
+def main(
+    domain_dir: str = "data/deterministic/blocksworld",
 ):
-    # parse pddl domain, problem -> mdp file =========================
-    mdp_text, translator = pddl_to_mdp(domain_file, problem_file)
-    with open("tmp/mdp.prism", "w") as f:
-        f.write(mdp_text)
+    problem_files = [f for f in os.listdir(domain_dir) if f.endswith(".pddl")]
+    problem_files.remove("domain.pddl")
+    policy_files = [f for f in os.listdir(domain_dir) if f.endswith(".json")]
+    property_files = [f for f in os.listdir(domain_dir) if f.endswith(".pctl")]
 
-    # apply policy to get actions -> dtmc file =======================
-    with open(policy_file, "r") as f:
-        policy = json.load(f)
-
-    dtmc_text = translator.generate_dtmc(policy)
-
-    with open("tmp/dtmc.prism", "w") as f:
-        f.write(dtmc_text)
-
-    # call prism on dtmc file, with property =========================
-    result = verify_property("tmp/dtmc.prism", property_file)
-
-    # print result
-    print(result)
-
-def main():
-    pass
-
-
+    for problem_file in problem_files:
+        for policy_file in policy_files:
+            for property_file in property_files:
+                result = run_single(domain_dir, problem_file, policy_file, property_file)
+    
 if __name__ == "__main__":
-    Fire(run_single)
+    Fire(main)
+
---
+++ run.py
@@ -0,0 +1,76 @@
+from fire import Fire
+import json
+import subprocess
+from translation import pddl_to_mdp
+import os
+
+def verify_property(dtmc_file: str, property_file: str) -> str:
+    """
+    dtmc_file: Path to the DTMC model file in PRISM format.
+    property_file: path to file containing property in 
+    """
+    # call prism on dtmc file, with property speicified in the file
+    with open(property_file, "r") as property_infile:
+        property = property_infile.read().strip()
+
+    results_file = "tmp/results.txt"
+    if not os.path.exists(results_file):
+        with open(results_file, "w") as f:
+            f.write("")
+
+    # Prepare command for subprocess
+    command = ["prism", dtmc_file, "-pctl", f"P=? [{property}]", f"-exportresults", f"{results_file}"]
+    command_text = " ".join(command)
+    print('COMMAND TEXT:', command_text)
+    output_data = subprocess.run(command, capture_output=True, text=True)
+
+    # Check return code
+    if output_data.returncode != 0:
+        print(f"PRISM returned {output_data.returncode} on inputs `{command}` with error:\n{output_data.stderr}\nAborting verify_property.")
+        print('ERROR:', output_data.stderr)
+        print('OUTPUT:', output_data.stdout)
+        return None
+    
+    # TODO perform post-processing:
+    # - Perform error checking (e.g. the model may be incomplete, or there may be warnings about its structure, or certain states are unreachable or unactionable)
+    # - Return results if there are no errors
+
+    with open(results_file, "r") as results_infile:
+        results_raw = results_infile.read().strip()
+        output_amount = results_raw.split()[1].strip()
+
+    return output_amount
+
+
+def run_single(
+    domain_dir: str = "data/deterministic/blocksworld/",
+    problem_file: str = "1.pddl",
+    policy_file: str = "all_on_table.json", 
+    property_file: str = "property.pctl",
+):
+    # parse pddl domain, problem -> mdp file =========================
+    domain_file = os.path.join(domain_dir, "domain.pddl")
+    problem_file = os.path.join(domain_dir, problem_file)
+    policy_file = os.path.join(domain_dir, policy_file)
+    property_file = os.path.join(domain_dir, property_file)
+    mdp_text, translator = pddl_to_mdp(domain_file, problem_file)
+    with open("tmp/mdp.prism", "w") as f:
+        f.write(mdp_text)
+
+    # apply policy to get actions -> dtmc file =======================
+    with open(policy_file, "r") as f:
+        policy = json.load(f)
+
+    dtmc_text = translator.generate_dtmc(policy)
+
+    with open("tmp/dtmc.prism", "w") as f:
+        f.write(dtmc_text)
+
+    # call prism on dtmc file, with property =========================
+    result = verify_property("tmp/dtmc.prism", property_file)
+
+    # print result
+    print(result)
+
+if __name__ == "__main__":
+    Fire(run_single)
